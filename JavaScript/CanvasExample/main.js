552cdceec3778d3420ba33241e451f2002308d47
yhashim <36962808+yhashim@users.noreply.github.com>
2019-11-12 11:21:55 -0500

    Edits

--- JavaScript/CanvasExample/main.js
+++ JavaScript/CanvasExample/main.js
@@ -1,6 +1,12 @@
-var image = null;
+var cards = {};
 
-
+function loadImages(){
+	for (var i = 2; i<=10; i++){
+		var img = new Image();
+  		img.src ="images/cards/clubs" + i + ".png"; 
+		cards['clubs' + i] = img;
+	}
+}
 
 function createImage(elem){
   var canvas = document.getElementById(elem);   // used to get the canvas to draw on it
@@ -9,10 +15,20 @@ function createImage(elem){
   
   var ctx = canvas.getContext("2d");
   var img = new Image();
-  img.src ="images/cards/clubs10.png";    // creates a blank SimpleImage with width and height
-  
-  
-  ctx.drawImage(img, 5, 5);           // this was like our print(img);  
+  // img.src ="images/cards/clubs10.png";    // creates a blank SimpleImage with width and height
+
+  //Make images crisper
+  // ctx.mozImageSmoothingEnabled = false;
+  // ctx.webkitImageSmoothingEnabled = false;
+  // ctx.msImageSmoothingEnabled = false;
+  // ctx.imageSmoothingEnabled = false;
+
+  var i = 10;
+  for (var c in cards){
+  	card = cards[c];
+  	ctx.drawImage(card, 5+i, 50, card.width/4, card.height/4);           // this was like our print(img); 
+  	i += 25; 
+  }
 }
 
 
--- JavaScript/GoFish/main.js
+++ JavaScript/GoFish/main.js
@@ -1,9 +1,10 @@
-const NUM_SUITS = 4;
+// const NUM_SUITS = 4;
 const NUM_FACES = 13;
 const PLAY = 1;
 const NUM_CARDS = 5;
 
 function init() {
+    loadAllImgs();
     var gameDone = false;
     var numPlayers = 0; // get from HTML pg how many wanted by player
     while (!gameDone) {
@@ -17,8 +18,10 @@ function init() {
             cps[i] = {};
             cps[i].name = "cp" + i;
             cps[i].hand = ["X", "X", "X", "X", "X"];
-            cps[i].numCards = 5;
+            cps[i].hiddenHand = [];
+            cps[i].numCards = 0;
             cps[i].points = 0;
+            cps[i].trait = getRandomTrait();
         }
         for (var i = 0; i < NUM_CARDS; i++) {
             var card = getCard();
@@ -26,12 +29,28 @@ function init() {
             player.numCards++;
         }
         for (var i = 0; i < numPlayers) {
-            console.log(cps[i].name + " " + cps[i].hand);
+            for (var i = 0; i < NUM_CARDS; i++) {
+                var card = getCard();
+                cps[i].hiddenHand[i] = card;
+                cps[i].numCards++;
+            }
+            while (pairs(cps[i].hand) == true) {
+                removePairs(cps[i].hand);
+                cps[i].numCards-=2;
+                console.log("Pairs removed.");
+                cps[i].hand = [null, null, null, null, null];
+                for (var i = 0; i<cps[i].numCards; i++){
+                    cps[i].hand[i] = "X";
+                }
+                console.log(cps[i].name + " " + cps[i].hand);
+                player.points++;
+                console.log("Points: " + player.points);
+            }
         }
         console.log(player.name + player.hand);
         while (pairs(player.hand) == true) {
             removePairs(player.hand);
-            player.numCards--;
+            player.numCards-=2;
             console.log("Pairs removed.");
             console.log("Hand: " + player.hand);
             player.points++;
@@ -50,4 +69,43 @@ function init() {
             }
         }
     }
+}
+
+function loadAllImgs(){
+
+}
+
+function getCard(){
+    var x  = Math.floor(Math.radnom() * NUM_FACES + 2);
+    if (x <= 10){
+        return "" + x;
+    } else if (x == 11){
+        return "J";
+    } else if (x == 12){
+        return "Q";
+    } else if (x == 13){
+        return "K";
+    } else {
+        return "A";
+    }
+}
+
+function checkPairs(hand){
+
+}
+
+function removePairs(hand){
+
+}
+
+function getNumWant(){
+
+}
+
+function getCPWant(){
+
+}
+
+function contain(cpWant, numWant){
+
 }
