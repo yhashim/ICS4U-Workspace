b1929073c73ba7b3e16e6eacd005fb357de5867b
yhashim <36962808+yhashim@users.noreply.github.com>
2020-01-17 11:11:24 -0500

    work

--- Unit 1/src/DynamicProgrammingAssignment/LongestZigZagSq.java
+++ Unit 1/src/DynamicProgrammingAssignment/LongestZigZagSq.java
@@ -1,76 +1,54 @@
 package DynamicProgrammingAssignment;
 
+import java.util.Arrays;
+
 public class LongestZigZagSq {
 
-	static String[][] dynamicProgramming;
+	static int[] diffs;
 	static int solution;
+	static int length;
 	
 	public static void main(String[] args) {
-		int[] sequence = {14, 3, 2, 7, 5, 5};
-		dynamicProgramming = new String[3][sequence.length];
-		solution = 0;
-			// num1, num2, ans
-		for (int i = 0; i<3; i++) {
-			for (int j = 0; j<dynamicProgramming.length; j++) {
-				dynamicProgramming[i][j] = null;
-			}
+		int[] sequence = {10, 22, 9, 50, 31, 60};
+		diffs = new int[sequence.length-1];
+		for (int i = 0; i<diffs.length; i++) {
+			diffs[i] = sequence[i+1]-sequence[i];
 		}
-		System.out.println(longestZigzagSq(sequence));
+		if (diffs.length<=1) {
+			System.out.println(sequence.length);
+		} 
+		else if (diffs.length==2) {
+			if (diffs[0]!=0) {
+				System.out.println(2);
+			} else {
+				System.out.println(1);
+			}
+		} 
+		else {
+			System.out.println(longestZigZagSq(sequence)+2);
+		}
 	}
 
-	public static int longestZigzagSq(int[] array) {
-		int length = 0;
-		int maxSq = 0;
-		if (array.length==0) {
-			return 0;
-		} else if (array.length==1) {
-			return 1;
-		} 
-		int[] a = new int[array.length];
-		for (int i = 0; i<array.length; i++) {
-			for (int j = i+1; j<array.length; j++) {
-				int one = array[i], two = array[j];
-				boolean before = checkDP(one, two);
-				if (before) {
-					if (solution < 0) {
-						/////////////////////
-						/////////////////////
-					} else if (solution == 0) {
-						/////////////////////
-						/////////////////////
-					} else if (solution > 0) {
-						/////////////////////
-						/////////////////////
-					}
-				} else {
-					// solve
-					/////////////////////
-					/////////////////////
-					if ((array[i] > array[j] && array[j]<array[j+1]) || (array[i] < array[j] && array[j]>array[j+1]) && a[i]<a[j]+1) {
-						a[i] = a[i] + 1;
-						if (a[i]>maxSq) {
-							maxSq = a[i];
-						}	
-					}
-				}
+	public static int longestZigZagSq(int[] array) {
+		int i;
+		for (i = 0; i+1<diffs.length; i++) {
+			if (diffs[i]!=0 && (Math.signum(diffs[i+1]*-1) == Math.signum(diffs[i]))) {
+				length++;
+			} else {
+				break;
 			}
 		}
-		return length;
-	}
-	
-	public static boolean checkDP(int one, int two) {
-		for (int i = 0; i<dynamicProgramming.length-2; i++) {
-			for (int j = 0; j<dynamicProgramming[0].length; j++) {
-				if (dynamicProgramming[i][j]==null) {
-					return false;
-				} else if (Integer.parseInt(dynamicProgramming[i][j])==one
-						&& Integer.parseInt(dynamicProgramming[i+1][j])==two) {
-					solution = Integer.parseInt(dynamicProgramming[i+1][j]);
-					return true;
-				}
+		if (i+1 == diffs.length-1) {
+			return length;
+		} else {
+			if (i+1>diffs.length) {
+				return 0;
+			} else {
+				diffs = Arrays.copyOfRange(diffs, i+1, diffs.length);
+				solution = Math.max(length, longestZigZagSq(diffs));
+				return solution;
 			}
 		}
-		return false;
-	}
+	} 
 
 }
